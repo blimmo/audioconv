c33a901a564cd5b7fdd076aeb0abf18854112490
Peter Strulo <peter.strulo@gmail.com>
2019-03-19 14:58:11 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+from configparser import ConfigParser
+import os
+from pathlib import Path
+import subprocess
+
+
+cfg = ConfigParser()
+cfg.read("config.ini")
+
+SOURCE = cfg["paths"]["source"]
+DEST = cfg["paths"]["dest"]
+
+
+def want_transcode(source_path: Path) -> bool:
+    """Determine whether the file at source_path should be transcoded."""
+    return source_path.suffix in cfg["filetypes"]["to transcode"].split(",")
+
+
+def transcode(in_path: Path, out_path: Path) -> None:
+    """Transcode the file at in_path and put the result at out_path"""
+    subprocess.run('ffmpeg -y -hide_banner -loglevel warning'
+                   f' -i "{in_path}" -b:a 128000 "{out_path}"')
+
+
+def to_dest(source_path: Path) -> Path:
+    return DEST / source_path.relative_to(SOURCE)
+
+
+def time(path: Path) -> int:
+    return path.stat().st_mtime
+
+
+def main():
+    good_dests = set()
+
+    # mirror source to dest with transcoding
+    for root, dirs, files in os.walk(SOURCE):
+        # mirror all folders
+        to_dest(Path(root)).mkdir(parents=True, exist_ok=True)
+        for file in files:
+            source_path = Path(root, file)
+            dest_path = to_dest(source_path)
+            if want_transcode(source_path):
+                dest_path = dest_path.with_suffix(".opus")
+
+            # source_path maps to this path so we want to keep it
+            good_dests.add(dest_path)
+
+            if dest_path.exists() and time(source_path) <= time(dest_path):
+                # already done this file on a previous run
+                # if a file that needs to be transcoded is updated it will be
+                # retranscoded, but linked files always have equal mtimes so
+                # will never get past this continue.
+                continue
+
+            if want_transcode(source_path):
+                print("Transcoding", source_path)
+                transcode(source_path, dest_path)
+            else:
+                print("Linking", source_path)
+                os.link(source_path, dest_path)
+
+    # remove old files in dest
+    for root, dirs, files in os.walk(DEST, topdown=False):
+        for file in files:
+            path = Path(root, file)
+            if path not in good_dests:
+                # nothing in source mapped to this so delete it
+                print("Removing", path)
+                path.unlink()
+
+        if len(os.listdir(root)) == 0:
+            # (now) empty dir
+            print("Removing", root)
+            Path(root).rmdir()
+
+
+if __name__ == "__main__":
+    main()
